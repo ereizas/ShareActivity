6f6cc8cf33257ac06fd2b1d307843240196eefc8
Eric Reizas <tuo17224@temple.edu>
2024-04-03 15:25:34 -0400

    Added toast warning for empty text

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +14,22 @@ class MainActivity : AppCompatActivity() {
 
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
-
+        val warningMessage = Toast.makeText(this,"Text cannot be empty",Toast.LENGTH_SHORT)
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-            val shareIntent = Intent.createChooser(Intent().apply {
-                action = Intent.ACTION_SEND
-                type="text/plain"
-                putExtra(Intent.EXTRA_TEXT, editText.text)
-            }, null)
-            startActivity(shareIntent)
+            if(editText.text.isEmpty())
+            {
+                warningMessage.show()
+            }
+            else
+            {
+                val shareIntent = Intent.createChooser(Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type="text/plain"
+                    putExtra(Intent.EXTRA_TEXT, editText.text)
+                }, null)
+                startActivity(shareIntent)
+            }
         }
     }
 }
